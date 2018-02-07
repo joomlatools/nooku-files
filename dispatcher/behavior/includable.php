9fc1213a43dd1e5579b2cb8459aa5f3b8af613e3
Arunas Mazeika <arunas@timble.net>
2018-02-07 17:04:37 +0100

    #49 Add includable behavior

---
+++ dispatcher/behavior/includable.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Joomlatools Framework - https://www.joomlatools.com/developer/framework/
+ *
+ * @copyright	Copyright (C) 2011 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://github.com/joomlatools/joomlatools-framework-files for the canonical source repository
+ */
+
+/**
+ * Includable Dispatcher Behavior
+ *
+ * @author  Arunas Mazeika <https://github.com/amazeika>
+ * @package Koowa\Component\Files
+ */
+class ComFilesDispatcherBehaviorIncludable extends KControllerBehaviorAbstract
+{
+    protected function _beforeInclude(KDispatcherContextInterface $context)
+    {
+        $context->append(array('params' => array('dispatcher' => $this->getMixer()->getIdentifier())));
+    }
+
+    /**
+     * Include the request
+     *
+     * Dispatch to a controller internally and handles back the context without sending the response
+     *
+     * @param DispatcherContext $context A dispatcher context object
+     * @return  mixed
+     */
+    protected function _actionInclude(KDispatcherContextInterface $context)
+    {
+        $params = $context->params;
+
+        $identifier = $params->dispatcher;
+
+        //Get the dispatcher identifier
+        if(is_string($identifier) && strpos($identifier, '.') === false )
+        {
+            $identifier            = $this->getIdentifier()->toArray();
+            $identifier['package'] = $identifier;
+        }
+
+        //Create the dispatcher
+        $config = array(
+            'request'  => clone $context->request,
+            'response' => $context->response,
+            'user'     => $context->user,
+        );
+
+        $dispatcher = $this->getObject($identifier, $config);
+
+        if ($query = $params->query)
+        {
+            if ($query instanceof KHttpMessageParameters) {
+                $query = $query->toArray();
+            }
+
+            if (!is_array($query)) throw new RuntimeException('Includable Dispatcher Behavior: $query should be an array');
+
+            $dispatcher->getRequest()->getQuery()->clear()->add($query);
+        }
+
+        if(!$dispatcher instanceof KDispatcherInterface)
+        {
+            throw new UnexpectedValueException(
+                'Dispatcher: '.get_class($dispatcher).' does not implement KDispatcherInterface'
+            );
+        }
+
+        $context = $dispatcher->getContext();
+
+        $context->included = true;
+
+        return $dispatcher->dispatch($context);
+    }
+
+    protected function _beforeSend(KDispatcherContextInterface $context)
+    {
+        if ($context->included)
+        {
+            if ($result = $context->result) {
+                $context->included_result = $result; // Keep a copy of the result
+            }
+
+            return false;
+        }
+    }
+
+    protected function _afterDispatch(KDispatcherContextInterface $context)
+    {
+        if ($context->included && is_null($context->result) && $context->included_result) {
+            $context->result = $context->included_result;
+        }
+    }
+}
