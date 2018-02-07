1cd8e7bd112ed73bf6aa7e82665a3bb531d05aaf
Arunas Mazeika <arunas@timble.net>
2018-02-07 16:38:08 +0100

    #49 Filter JSON URLs

--- view/json.php
+++ view/json.php
@@ -15,6 +15,14 @@
  */
 class ComFilesViewJson extends KViewJson
 {
+    protected $_filter;
+
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array('filter' => 'com:files.template.filter.url'));
+        parent::_initialize($config);
+    }
+
     protected function _renderData()
     {
         $output = parent::_renderData();
@@ -32,5 +40,48 @@ class ComFilesViewJson extends KViewJson
         }
 
         return $output;
+    }
+
+    /**
+     * Converts links in an array from relative to absolute
+     *
+     * @param array $array Source array
+     */
+    protected function _processLinks(array &$array)
+    {
+        $base = $this->getUrl()->toString(KHttpUrl::AUTHORITY);
+
+        foreach ($array as $key => &$value)
+        {
+            if (is_array($value)) {
+                $this->_processLinks($value);
+            }
+            elseif ($key === 'href')
+            {
+                if (substr($value, 0, 4) !== 'http') {
+                    $array[$key] = $base.$value;
+                }
+            }
+            elseif ($key === 'uri')
+            {
+                // Expose URL from URI using template filter
+                $parts = explode('://', $value);
+                $url = sprintf('files://%s/%s', $parts[0], $parts[1]);
+                $this->_getFilter()->filter($url);
+                $array[$key] = $url;
+            }
+            elseif (in_array($key, $this->_text_fields)) {
+                $array[$key] = $this->_processText($value);
+            }
+        }
+    }
+
+    protected function _getFilter()
+    {
+        if (!$this->_filter) {
+            $this->_filter = $this->getObject($this->getConfig()->filter);
+        }
+
+        return $this->_filter;
     }
 }
