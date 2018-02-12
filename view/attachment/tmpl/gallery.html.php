c6ee8f092d97b75773426f0c579aa0c07f37eeaa
Arunas Mazeika <arunas@timble.net>
2018-02-12 16:07:13 +0100

    #49 Add missing wrapping containers

--- view/attachment/tmpl/gallery.html.php
+++ view/attachment/tmpl/gallery.html.php
@@ -12,39 +12,43 @@
 <? $size = isset($size) ?: null ?>
 
 <? if ($file = $attachment->file): ?>
-    <a class="koowa_media__item__link" href="<?= route($url, true, false) ?>">
-        <div class="koowa_media__item__content-holder">
-            <? if ($file->storage->isImage()): ?>
-                <div class="koowa_media__item__image">
-                    <? if ($file->storage->isThumbnailable() && ($thumbnail = $file->storage->getThumbnail($size))): ?>
-                        <div class="koowa_media__item__thumbnail">
-                            <img src="<?= sprintf('files://%s/%s', $thumbnail->container, $thumbnail->path) ?>"/>
-                        </div>
-                    <? else: ?>
-                        <div class="koowa_media__item__icon">
-                            <span class="k-icon-document-image k-icon--size-xlarge"></span>
-                        </div>
-                    <? endif ?>
-                </div>
-            <? else: ?>
-                <div class="koowa_media__item__icon">
-                    <span class="k-icon-document-default k-icon--size-xlarge"></span>
-                </div>
-            <? endif ?>
-            <div class="koowa_header koowa_media__item__label">
-                <div class="koowa_header__item koowa_header__item--title_container">
-                    <div class="koowa_wrapped_content">
-                        <div class="whitespace_preserver">
-                            <div class="overflow_container">
-                                <span class="js-gallery-caption" style="display: none">
+<div class="koowa_media__item">
+    <div class="koowa_media__item__content file">
+        <a class="koowa_media__item__link" href="<?= route($url, true, false) ?>">
+            <div class="koowa_media__item__content-holder">
+                <? if ($file->storage->isImage()): ?>
+                    <div class="koowa_media__item__image">
+                        <? if ($file->storage->isThumbnailable() && ($thumbnail = $file->storage->getThumbnail($size))): ?>
+                            <div class="koowa_media__item__thumbnail">
+                                <img src="<?= sprintf('files://%s/%s', $thumbnail->container, $thumbnail->path) ?>"/>
+                            </div>
+                        <? else: ?>
+                            <div class="koowa_media__item__icon">
+                                <span class="k-icon-document-image k-icon--size-xlarge"></span>
+                            </div>
+                        <? endif ?>
+                    </div>
+                <? else: ?>
+                    <div class="koowa_media__item__icon">
+                        <span class="k-icon-document-default k-icon--size-xlarge"></span>
+                    </div>
+                <? endif ?>
+                <div class="koowa_header koowa_media__item__label">
+                    <div class="koowa_header__item koowa_header__item--title_container">
+                        <div class="koowa_wrapped_content">
+                            <div class="whitespace_preserver">
+                                <div class="overflow_container">
+                                    <span class="js-gallery-caption" style="display: none">
+                                        <?= escape($file->name) ?>
+                                    </span>
                                     <?= escape($file->name) ?>
-                                </span>
-                                <?= escape($file->name) ?>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </a>
+        </a>
+    </div>
+</div>
 <? endif ?>
